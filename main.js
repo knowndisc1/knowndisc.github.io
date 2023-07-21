96aa895a01d6b083583c8a563746cf8432cf9b25
knowndisc <sun.pauly@gmail.com>
2023-07-21 11:31:14 -0500

    More

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
-import * as THREE from 'three';
-
-...
+const track = document.getElementById("image-track");
+window.onmousedown = e =>{
+    track.dataset.mouseDownAt = e.clientX;
+    track.dataset.prevPercentage = track.dataset.percentage;
+}
+window.onmousemove = e =>{
+    if (track.dataset.mouseDownAt === "0") return;
+    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX, 
+    maxDelta = window.innerwidth/2;
+    const percentage = (mouseDelta/maxDelta) * -100;
+    track.dataset.percentage = nextPercentage;
+    track.style.transform = 'translate(${percentage}%, -50%)'
+    track.animate({transform: 'translate(${nextPercentage}%, -50%)' }, {duration:1200, fill:"forwards"});
+    image.style.objectPosition = '${nextPercentage + 100}% 50%';
+    image.animate({objectPosition: '${100 + nextPercentage} % center'}, {duration: 1200, fill: "forwards"});
+}
+window.onmouseup = () => {
+    track.dataset.mouseDownAt = "0";
+    track.dataset.prevPercentage = track.dataset.percentage;
+}
